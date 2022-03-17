24b5d4c975c69b4635de7284fa562b6788c945e7
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-17 13:16:24 +0400

    Access modifiers

---
+++ NEW_TASK/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace NEW_TASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  StringBuilder stringBuilder = new StringBuilder();
+
+            //Admin admin = new Admin("hello","ilkinyusubov",1234,true);
+            //admin.Information();
+
+            User user = new User("ilkinyuh");
+            user.Username();
+            
+
+
+            //User user = new User("ilkin",1234);
+            //user.userinfo();
+
+
+
+
+
+
+        }
+    }
+}
